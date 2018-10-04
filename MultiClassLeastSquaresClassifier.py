b3c85d275d8c97002520846974132daa3083c437
rahulsai2341 <rahulsai2341@gmail.com>
2018-10-04 12:56:32 -0400

    almost completed code when input is divided 50:50 for training and testing

--- MultiClassLeastSquaresClassifier.py
+++ MultiClassLeastSquaresClassifier.py
@@ -1,16 +1,6 @@
 import numpy as np
 from sklearn import datasets
-import pandas
 import matplotlib
-
-#url = r"C:\Users\Rahul\Documents\UFL\Courses\MIS\Homework4\iris.csv"
-#names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "class"]
-#dataframe = pandas.read_csv(url, names=names, engine='python')
-#print(dataframe)
-#dataframe_training = dataframe.iloc[:75,0:5]
-#dataframe_test = dataframe.iloc[75:,0:4]
-#print(dataframe_training)
-#print(dataframe_test)
 
 iris = datasets.load_iris()
 X = iris.data
@@ -18,9 +8,11 @@ Y = iris.target
 X_train = X[:75].T
 X_test = X[75:]
 Y_train = Y[:75]
-#print(Y_train.shape)
+#print(Y_train)
 Y_train_mat = np.zeros((3,75))
 #print(X_train.shape)
+
+
 #Convert single column of Y's to 3*75 matrix
 for x in range(0,75):
     if Y_train[x]==0:
@@ -31,13 +23,50 @@ for x in range(0,75):
         Y_train_mat[2][x]=1
 #print(Y_train_mat.shape)
 Y_train_mat  = np.asmatrix(Y_train_mat)
+
+
 #added row of ones at the end
 X_train = np.asmatrix(np.vstack((X_train,np.ones((1,75)))))
 #print((X_train[:,1]))
 #print(np.transpose(Y_train_mat[:,1]))
-xy = np.zeros((5,3))
+xyt = np.zeros((5,3))
+
+
 #Finding sum of XY^t for all patterns
 for i in range(0,75):
-    xy = xy + np.matmul(X_train[:,i],(np.transpose(Y_train_mat[:,i])))
-print(xy)
+    xyt = xyt + np.matmul(X_train[:,i],(np.transpose(Y_train_mat[:,i])))
+#print("xyt = ")
+#print(xyt)
 
+# finding inverse of sum of x.x^t for all input patterns
+xxt = np.zeros((5,5))
+for i in range(0,75):
+    xxt = xxt + np.matmul(X_train[:,i],np.transpose(X_train[:,i]))
+#print("xxt = ")
+#print(xxt)
+a = np.diagflat([0.001,0.001,0.001,0.001,0.001])
+#print("a = ")
+#print(a)
+xxt_inv = np.linalg.inv(xxt+a)
+#print("xxt_inv = ")
+#print(xxt_inv)
+
+#find weight matrix
+W = np.matmul(xxt_inv,xyt)#5X3
+#print("W = ")
+#print(W)
+W1 = W[:,0]#5X1
+W2 = W[:,1]
+W3 = W[:,2]
+#print("X_test = ")
+#print(X_test)#75X4
+out = np.zeros((3))
+
+#Testing
+X_test_1 = np.transpose(np.asmatrix(np.hstack((X_test[1,:],1))))
+#print(X_test_1)
+#print(X_test_1.shape)
+out[0] = np.matmul(np.transpose(W1),X_test_1)
+out[1] = np.matmul(np.transpose(W2),X_test_1)
+out[2] = np.matmul(np.transpose(W3),X_test_1)
+print(out)
